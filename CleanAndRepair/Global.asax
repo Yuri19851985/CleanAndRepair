dfe7280264c0b5fbee1c4cbc47f0e7f5dc7b21da
Yuri ZH <yuri-zhurakovski@yandex.ru>
2018-12-17 23:41:13 +0300

    first commit

---
+++ CleanAndRepair/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CleanAndRepair.MvcApplication" Language="C#" %>
---
+++ CleanAndRepair/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CleanAndRepair.Startup))]
+namespace CleanAndRepair
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
