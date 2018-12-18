6ccf135b9aa0497951a235557dfde9d130a25713
Yuri ZH <yuri-zhurakovski@yandex.ru>
2018-12-18 23:33:43 +0300

    model

---
+++ CleanAndRepair/Models/Professioanal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CleanAndRepair.Models
+{
+    public class Professioanal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Service> Services { get; set; }
+    }
+}
