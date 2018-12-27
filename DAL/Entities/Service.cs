ce4790d01bc73c0c6b03c78cb110d0ce2dc57d72
Yuri ZH <yuri-zhurakovski@yandex.ru>
2018-12-27 23:20:18 +0300

    27122018

---
+++ DAL/Entities/Service.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CleanAndRepair.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Professioanal> Workers { get; set; }
+    }
+}
